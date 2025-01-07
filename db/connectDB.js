2de4cc7452c7669bc12e151821f16f4f64a3cb54
shamil <shamilm2324@gmail.com>
2025-01-07 10:39:25 +0530

    feat:DB Changed To Atlas

--- db/connectDB.js
+++ db/connectDB.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose')
 
 const connectDB=async()=>{
     try {
-        const connect=await mongoose.connect('mongodb://localhost:27017/Ecommerce')
+        const connect=await mongoose.connect(process.env.MONGO_URL)
+        console.log('MongoDB Connected...')
     } catch (error) {
         console.log(error);
         process.exit(1)
